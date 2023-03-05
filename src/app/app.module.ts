71b27b13c1b30f04d9cea2d06b5fd6b6c9989533
Martin Amaya <martin.amaya@Martins-MacBook-Pro.local>
2023-03-04 23:29:28 -0300

    Portfolio en Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './componentes/navigation/navigation.component';
+import { HeroComponent } from './componentes/hero/hero.component';
+import { AboutmeComponent } from './componentes/about-me/aboutme.component';
+import { ProjectsComponent } from './componentes/projects/projects.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { ContactComponent } from './componentes/contact/contact.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HeroComponent,
+    AboutmeComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
